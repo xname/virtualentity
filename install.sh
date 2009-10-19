f98fdf099f9e950fd37bb339f4d3bb504d59de93
xname <root@xname.cc>
2009-10-19 02:11:41 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PREFIX=/usr
+
+echo 'installing virtual entity in ' $PREFIX '...'
+echo 'most likely you will have to be superuser to do this'
+mkdir   -p $PREFIX/share/soul/
+install -m 644 src/*.py $PREFIX/share/soul
+install -m 755 src/soul $PREFIX/bin/soul
+install -m 644 AUTHORS COPYING INSTALL metadata_sets README TODO USAGE virtualities $PREFIX/share/soul
+echo 'hopefully done' 
+

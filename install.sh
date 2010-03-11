aa29f4204bf7bbd346d14d1b03ca5d7cfafde6ba
xname <xname@xname.cc>
2010-03-11 21:13:54 +0100

    install man page

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ mkdir   -p $PREFIX/share/soul/
 install -m 644 src/*.py $PREFIX/share/soul
 install -m 755 src/soul $PREFIX/bin/soul
 install -m 644 AUTHORS COPYING INSTALL metadata_sets README TODO USAGE virtualities $PREFIX/share/soul
+install -m 644 soul.1.gz $PREFIX/share/man/man1/
 echo 'hopefully done' 
 

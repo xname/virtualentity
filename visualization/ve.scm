a59bb628e102dce66d6688872be1d4cafcc3bc1c
Gabor Papp <gabor.papp@gmail.com>
2011-02-15 14:52:07 +0100

    initial fluxus visualization

---
+++ visualization/ve.scm
@@ -0,0 +1,19 @@
+(require "vedb.ss")
+
+(clear)
+
+(define p (build-particles (length souls)))
+
+(with-primitive p
+    (pdata-map!
+        (lambda (p)
+            (crndvec))
+        "p")
+    (pdata-index-map!
+        (lambda (i c)
+            (case (soul-substance (list-ref souls i))
+                [(A) #(0 0 1)]
+                [(V) #(1 0 0)]
+                [(T) #(1 1 1)]
+                [(I) #(1 0 1)]))
+        "c")) 

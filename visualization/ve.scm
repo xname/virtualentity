0887a9bb76707b5904970859631c9865a01d0202
Gabor Papp <gabor.papp@gmail.com>
2011-02-20 11:00:46 +0100

    ve spheres

--- visualization/ve.scm
+++ visualization/ve.scm
@@ -1,24 +1,37 @@
 ; virtual entity visualization 
 
+(require racket/class)
 (require "vedb.ss")
 
 (clear)
 
-(define p (build-particles (length souls)))
+(define (clamp v a b)
+  (cond [(< v a) a]
+		[(> v b) b]
+		[else v]))
 
-(with-primitive p
-    (pdata-index-map!
-        (lambda (i p)
-            (random-seed (bitwise-and (string->number (soul-md5 (list-ref souls i)) 16)
+(define ve%
+  (class object%
+         (init-field s) ; soul
+
+         (define p (build-icosphere 3))
+         (with-primitive p
+            (random-seed (bitwise-and (string->number (soul-md5 s) 16)
                          (sub1 (arithmetic-shift 1 31))))
-            (crndvec))
-        "p")
-    (pdata-index-map!
-        (lambda (i c)
-            (case (soul-substance (list-ref souls i))
-                [(A) #(0 0 1)]
-                [(V) #(1 0 0)]
-                [(T) #(1 1 1)]
-                [(I) #(1 0 1)]))
-        "c")) 
+            (translate (vmul (crndvec) 7))
+            (scale .3)
+            (let ([col (rgb->hsv
+							(case (soul-substance s)
+								[(A) #(0 0 1)]
+								[(V) #(1 0 0)]
+								[(T) #(1 1 1)]
+								[(I) #(1 0 1)]))])
+			  (colour (hsv->rgb col))))
+
+         (super-new)))
+
+(define ve-list (map
+                  (lambda (s)
+                    (make-object ve% s))
+                  souls))
 
--- visualization/vedb.ss
+++ visualization/vedb.ss
@@ -4,9 +4,12 @@
 (require (file "mysql/main.ss"))
 
 (provide souls
-		 (struct-out soul))
+		 (struct-out soul)
+		 aliases
+		 (struct-out alias))
 
 (define-struct soul (id md5 substance password filename) #:transparent)
+(define-struct alias (id soul-id md5) #:transparent)
 
 (define ve-host "db.virtualentity.org")
 (define ve-port 3306)
@@ -22,6 +25,16 @@
 	  (lambda (id md5 substance password filename)
 		(make-soul id md5 (string->symbol substance) password filename))
 	  "select id,md5,substance,password,filename from souls;"))
+
+(define aliases (make-hash))
+
+(for-each
+	(lambda (a)
+		(hash-set! aliases (alias-soul-id a) a))
+	(query/map
+	  (lambda (id soul-id md5)
+		(make-alias id soul-id md5))
+	  "select id,soul_id,md5 from aliases;"))
 
 (close-connection! ve-connection)
 
